0fb4fa638520b115c52197b107cb0c1f8bc1161f
Yujun CHEN <YChen32@slb.com>
2025-01-03 13:31:32 +0800

    long endurance testing clients

--- src/CppClientSample/CppClientSample.cpp
+++ src/CppClientSample/CppClientSample.cpp
@@ -4,18 +4,113 @@
 #include <iostream>
 #include "../MemoryKVLib/MemoryKV.h"
 
+bool ParseInput(const std::string& input, std::string& mode, int& count, int& start_index)
+{
+    size_t start = 0, end;
+    try {
+        // Loop through the string and split by space
+        if ((end = input.find(' ', start)) != std::string::npos)
+        {
+            mode = input.substr(start, end - start);
+            start = end + 1;
+        }
+        else
+        {
+            mode = input;
+            return true;
+        }
+
+        if ((end = input.find(' ', start)) != std::string::npos)
+        {
+            auto temp = input.substr(start, end - start);
+            count = std::stoi(temp);
+            start = end + 1;
+        }
+
+        {
+            auto temp = input.substr(start);
+            start_index = std::stoi(temp);
+        }
+    }
+    catch (...)
+    {
+        return false;
+    }
+    return true;
+}
+
+void TestPut(MemoryKV& kv, int count, int start_index)
+{
+    for (int i = 0; i < count; i++)
+    {
+        std::wstringstream ss;
+        ss << L"key" << (i + start_index);
+        auto key = ss.str();
+        ss.str(std::wstring());
+        ss << L"value" << (i + start_index);
+        auto value = ss.str();
+        kv.Put(key, value);
+        Sleep(100);
+    }
+    std::cout << "test put " << count << " " << start_index << " done.\n";
+}
+
+void TestGet(MemoryKV& kv, int count, int start_index)
+{
+    for (int i = 0; i < count; i++)
+    {
+        std::wstringstream ss;
+        ss << L"key" << (i + start_index);
+        auto key = ss.str();
+        auto real_value = kv.Get(key);
+        ss.str(std::wstring());
+        ss << L"value" << (i + start_index);
+        auto expected_value = ss.str();
+        if(real_value!=expected_value)
+        {
+            std::wcout << L"key=" << key << L" value changed. expected=" << expected_value << ",real=" << real_value << std::endl;
+        }
+        Sleep(100);
+    }
+    std::cout << "test get " << count << " " << start_index << " done.\n";
+}
+
 int main()
 {
     MemoryKV kv(L"cppclient");
+    std::wcout << L"start testing.\n";
+    while (true)
+    {
+        std::string input;
+        std::wcout << L"Usages:\n"
+            << L"\'put 100 1\', put 100 values from the given key sequence, using predefined key (key1, key2, ...) \n"
+            << L"\'get 100 5\', get 100 values from the given key sequence, using predefined key (key5, key6, ...)\n"
+            << L"\'exit\', to exit testing\n";
+        std::getline(std::cin, input);
+        std::string mode;
+        int count;
+        int startIndex;
+        if(!ParseInput(input, mode, count, startIndex))
+            continue;
 
-    kv.Put(L"key1", L"1234567");
-    std::cout << "Put key1\n";
-    kv.Put(L"key2", L"abcdefg");
-    std::cout << "Put key2\n";
-
-    Sleep(500);
-    auto val = kv.Get(L"key1");
-    std::wcout << "Get key1="<<val<<std::endl;
+        if(mode =="put")
+        {
+            TestPut(kv, count, startIndex);
+        }
+        else if(mode=="get")
+        {
+            TestGet(kv, count, startIndex);
+        }
+        else if (mode=="exit")
+        {
+            std::wcout << L"exit testing\n";
+            return 1;
+        }
+        else
+        {
+            std::wcout << L"wrong input\n";
+        }
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- src/readme.md
+++ src/readme.md
@@ -1,12 +1,20 @@
 # remaining issues:
 
 ## 1 resize problem
+1. take small size at beginning, and resize when needed - done
+1. don't copy data when resizing, extend data section - done
 1. mark the removed slots
 1. reuse the removed slots
+1. shrink the size when no need
 
 ## 2 auto sync
 1. The new created MMF is deleted before server synched it
 1. Any new put data blocks are in MMF but not in key-position hashmap so cant be queried
+
+
+## Put function
+1. Update same key again should not create a new block (if the key is put by another client before, then loop it if search fails)
+1. The first client expand a MMF, the second client should sync before using it (put/get)
 
 ## 3 use hash code not loop to query
 1. fully done
@@ -19,7 +27,7 @@
 1. add client ID - done
 
 ## code management
-1. C# separate library project
+1. C# separate library project - done
 
 ## Misc
 1. save current snapshot
